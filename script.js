29a9fc9d2ea880aadd070e83bb7c4f9b6306240b
NateBess <NathanBessler@outlook.com>
2021-10-16 12:36:09 -0500

    Rock, Paper, Scissors! *Completed

--- script.js
+++ script.js
@@ -1,8 +1,78 @@
+
+function computerPlay() {
+    randomNumber = Math.floor(Math.random()*3);
+    console.log(randomNumber);
+
+    if (randomNumber === 0) {return 'rock';}
+    else if (randomNumber === 1) {return 'paper';}
+    else if (randomNumber === 2) {return 'scissors';}
+    else {console.log("Error Occurred! Try Again!");}
+}
+
+
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-    
+    let whoWon = null;
+    if (playerSelection === computerSelection) {
+        console.log("There was a Tie!");
+    }
+    else if (playerSelection !== computerSelection) {
+        if (playerSelection === 'paper' && computerSelection === 'rock') {
+            console.log("Player Wins! Paper Beats Rock!");
+            whoWon = true;
+        }
+        else if (computerSelection === 'paper' && playerSelection === 'rock') {
+            console.log("Computer Wins! Paper Beats Rock!");
+            whoWon = false;
+        }
+
+        if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            console.log("Player Wins! Rock Beats Scissors!");
+            whoWon = true;
+        }
+        else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+            console.log("Computer Wins! Rock Beats Scissors!");
+            whoWon = false;
+        }
+
+        if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            console.log("Player Wins! Scissors Beats Paper!");
+            whoWon = true;
+        }
+        else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+            console.log("Computer Wins! Scissors Beats Paper!");
+            whoWon = false;
+        }
+    }
+    return whoWon;
   }
-  
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (true){
+        let playerSelection = prompt("Enter 'rock, paper, or scissors");
+        let computerSelection = computerPlay();
+        whoWon = playRound(playerSelection, computerSelection);
+        if (whoWon === true) {
+            playerScore += 1;
+        }
+        else if (whoWon === false) {
+            computerScore += 1;
+        }
+        console.log(`\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore} \n`);
+
+        if (playerScore === 3) {
+            console.log("\nPlayer WON THE GAME!!!!")
+            break
+        }
+        if (computerScore === 3) {
+            console.log("\nComputer WON THE GAME!!!!")
+            break
+        }
+    }
+}
+
+playGame()
+
+
