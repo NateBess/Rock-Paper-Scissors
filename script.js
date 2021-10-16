8b07e6336677a1471a094598d3cefbcd134951a3
NateBess <NathanBessler@outlook.com>
2021-10-16 10:53:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
